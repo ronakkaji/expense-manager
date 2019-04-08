2d1816fbdea6dff1bf356297b785f843c1a756b1
Ronak Kaji <ronak.h.kaji@gmail.com>
2019-04-08 19:03:48 +0200

    Integrated mock API endpoint for login form.

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
 import "./styles.css";
+
+$(".submit").click(function() {
+	let email = $("#email").val();
+	let password = $("#password").val();
+	$.ajax({
+		url: "https://reqres.in/api/register",
+		method: "POST",
+		data: {
+			email: email,
+			password: password
+		},
+		success: function (response) {
+			$("#result").html(`The token value is: ${response.token}`);
+		},
+		error: function(error){
+			$("#result").html(`Something went wrong. Please try again.`);
+		}
+	})
+});
